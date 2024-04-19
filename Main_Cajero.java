92b22c3db03da7dec8ebdc7dbccfa7aa513fef0e
daw129 <Biriato>
2024-04-19 09:43:44 +0200

    V.1.04

--- Main_Cajero.java
+++ Main_Cajero.java
@@ -4,15 +4,17 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.Scanner;
 
+import static Proyecto_CODE.AdminCuenta.*;
 import static Proyecto_CODE.Operacion.cuentas;
-
 public class Main_Cajero {
     static Scanner sc = new Scanner(System.in);
     static Scanner sc2 = new Scanner(System.in);
     static int id=1;
     static  int numcuenta =1;
     public static void main(String[] args) {
+        Operacion operacion = new Operacion();
         int opcion=1;
+        int numacc=0;
         boolean existe = false;
         do{
             System.out.println("Seleciona la opcion que quieras realizar");
@@ -33,20 +35,33 @@ public class Main_Cajero {
                     }
                     break;
                 case 2:
-                    System.out.println("Introduce el numero de cuenta y el dni");
+                    System.out.println("Introduce el numero de cuenta");
+                    numacc = sc.nextInt();
+                    existe = operacion.Verificar(numacc);
                     if(existe){
                         System.out.println("Que operacion quieres realizar sobre esta cuenta");
                         System.out.println("1)Ingreso 2)Retirada 3)Traspaso 4)Ver Saldo de la cuenta");
                         switch (opcion){
                             case 1:
+                                operacion.Ingreso(numacc);
+                                existe = false;
                                 break;
                             case 2:
+                                operacion.Retirada(numacc);
+                                existe = false;
                                 break;
                             case 3:
+                                operacion.Traspaso(numacc);
+                                existe = false;
                                 break;
                             case 4:
+                                operacion.VerSaldo(numacc);
+                                existe = false;
                                 break;
                         }
+                    }else {
+                        System.out.println("El numero de cuenta introducido no existe");
+
                     }
 
                     break;
@@ -55,54 +70,7 @@ public class Main_Cajero {
         }while(opcion!=0);
 
     }
-    public static void AñadirCuenta(){
-        System.out.println("Introduce un DNI");
-        String DNI=sc2.nextLine();
-        System.out.println("Saldo base de la cuenta");
-        int saldob = sc.nextInt();
-        System.out.println("Propietario de la cuenta");
-        String prop = sc2.nextLine();
-        cuentas.put(id,new CuentaBanco(DNI,numcuenta,saldob,prop));
 
-    }
-
-    public  static void EliminarCuenta(){
-        System.out.println("Introduce el numero de cuenta que quieres eliminar");
-        int ver = sc.nextInt();
-        int clave=0;
-
-        for (Map.Entry<Integer,CuentaBanco> c : cuentas.entrySet()) {
-            if (ver == c.getValue().getNumCuenta()) {
-                   clave=c.getKey();
-            }
-        }
-        cuentas.remove(clave);
-
-    }
-    public static  void ModificarCuenta(){
-        int clave = 0;
-        String p="";
-            System.out.println("Introduce el numero de cuenta que quieres modificar");
-            int acu = sc.nextInt();
-            for (Map.Entry<Integer, CuentaBanco> c : cuentas.entrySet()) {
-                if (acu == c.getValue().getNumCuenta()) {
-                    clave = c.getKey();
-                } else {
-                    clave = 0;
-                }
-            }
-            if(clave!=0){
-                System.out.println("Introduce el nuevo propietario");
-                p=sc2.nextLine();
-                for (Map.Entry<Integer, CuentaBanco> c : cuentas.entrySet()) {
-                    if (clave==c.getKey()) {
-                        c.getValue().setPropietario(p);
-                    }
-                }
-            }
-
-
-    }
 
 
 }
--- Operacion.java
+++ Operacion.java
@@ -8,13 +8,11 @@ import java.util.Scanner;
 public class Operacion {
     static Hashtable<Integer , CuentaBanco> cuentas= new Hashtable<>();
     static Scanner sc = new Scanner(System.in);
-    private String TipoOperacion;
+
 
     //Constructores
 
-    public Operacion(String tipoOperacion) {
-        this.TipoOperacion = tipoOperacion;
-
+    public Operacion() {
     }
     //Metodos de la clase
     public void Ingreso(int clave){
@@ -51,36 +49,36 @@ public class Operacion {
     }
     public void Traspaso(int clave){
         int retirada=1;
-        System.out.println("Introduce el numero de cuanta a la que quieras realizar el traspaso");
-        int numc = sc.nextInt();
-        do {
+        while (retirada!=0){
             System.out.println("Introduce la cantidad a traspasar o 0 para salir");
             retirada = sc.nextInt();
-        }while(retirada<0);
-
-
-
-        while (retirada!=0){
-
             for (Map.Entry<Integer, CuentaBanco> c : cuentas.entrySet()) {
                 if (clave == c.getKey()) {
                     c.getValue().setSaldo(c.getValue().getSaldo() - retirada);
 
                     System.out.println("Operacion realizada con exito");
-                    if (c.getValue().getSaldo() < 0) {
-                        c.getValue().setSaldo(0);
-                    }
                 }
             }
 
         }
+        }
 
-
-
+    public void VerSaldo(int clave){
+        for (Map.Entry<Integer, CuentaBanco> c : cuentas.entrySet()) {
+            if (clave == c.getKey()) {
+                System.out.println(c.toString());
+            }
+        }
 
     }
-    public void VerSaldo(int clave){
-
+    public boolean Verificar(int numacc){
+        for (Map.Entry<Integer, CuentaBanco> c : cuentas.entrySet()) {
+            if (numacc == c.getValue().getNumCuenta()) {
+               numacc = c.getKey();
+               break;
+            }
+        }
+        return cuentas.containsKey(numacc);
     }
 
 }
