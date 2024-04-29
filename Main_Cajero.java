fcf445780bf9a778bfdaeb049d8211058863f35b
daw129 <Biriato>
2024-04-29 09:02:35 +0200

    V1.2(Se arregla la creacion de cuentas)

--- Main_Cajero.java
+++ Main_Cajero.java
@@ -24,58 +24,64 @@ public class Main_Cajero {
         int opcion=1;
         int numacc=0;
         boolean existe = false;
-        do{
-            System.out.println("Seleciona la opcion que quieras realizar");
-            System.out.println("0)Salir 1)Administar Cuenta 2 Operaciones con su cuenta");
-            opcion=sc.nextInt();
-            switch (opcion){
-                case 1:
-                    System.out.println("Seleccionaste Administrar Cuenta");
-                    System.out.println("1)Alta de cuenta 2)Dar de baja una cuenta 3)Modificar cuenta");
-                    opcion = sc.nextInt();
-                    switch (opcion){
-                        case 1: AñadirCuenta();
-                            break;
-                        case 2: EliminarCuenta();
-                            break;
-                        case 3: ModificarCuenta();
-                            break;
-                    }
-                    break;
-                case 2:
-                    System.out.println("Introduce el numero de cuenta");
-                    numacc = sc.nextInt();
-                    existe = operacion.Verificar(numacc);
-                    if(existe){
-                        System.out.println("Que operacion quieres realizar sobre esta cuenta");
-                        System.out.println("1)Ingreso 2)Retirada 3)Traspaso 4)Ver Saldo de la cuenta");
+        try {
+            do {
+                System.out.println("Seleciona la opcion que quieras realizar");
+                System.out.println("0)Salir 1)Administar Cuenta 2 Operaciones con su cuenta");
+                opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Seleccionaste Administrar Cuenta");
+                        System.out.println("1)Alta de cuenta 2)Dar de baja una cuenta 3)Modificar cuenta");
                         opcion = sc.nextInt();
-                        switch (opcion){
+                        switch (opcion) {
                             case 1:
-                                operacion.Ingreso(numacc);
-                                existe = false;
+                                AñadirCuenta();
                                 break;
                             case 2:
-                                operacion.Retirada(numacc);
-                                existe = false;
+                                EliminarCuenta();
                                 break;
                             case 3:
-                                operacion.Traspaso(numacc);
-                                existe = false;
-                                break;
-                            case 4:
-                                operacion.VerSaldo(numacc);
-                                existe = false;
+                                ModificarCuenta();
                                 break;
                         }
-                    }else {
-                        System.out.println("El numero de cuenta introducido no existe");
+                        break;
+                    case 2:
+                        System.out.println("Introduce el numero de cuenta");
+                        numacc = sc.nextInt();
+                        existe = operacion.Verificar(numacc);
+                        if (existe) {
+                            System.out.println("Que operacion quieres realizar sobre esta cuenta");
+                            System.out.println("1)Ingreso 2)Retirada 3)Traspaso 4)Ver Saldo de la cuenta");
+                            opcion = sc.nextInt();
+                            switch (opcion) {
+                                case 1:
+                                    operacion.Ingreso(numacc);
+                                    existe = false;
+                                    break;
+                                case 2:
+                                    operacion.Retirada(numacc);
+                                    existe = false;
+                                    break;
+                                case 3:
+                                    operacion.Traspaso(numacc);
+                                    existe = false;
+                                    break;
+                                case 4:
+                                    operacion.VerSaldo(numacc);
+                                    existe = false;
+                                    break;
+                            }
+                        } else {
+                            System.out.println("El numero de cuenta introducido no existe");
 
-                    }
-                    break;
-            }
-        }while(opcion!=0);
-
+                        }
+                        break;
+                }
+            } while (opcion != 0);
+        }catch (Exception e){
+            System.out.println("La opcion seleccionada no existe");
+        }
     }
 
 
